77904a168601d3281dc3055acdc2de6385abf45c
zach-snyder <zach@Zacharys-Laptop>
2024-08-30 18:53:24 -0600

    testing_whatpulls

--- inventory_app/views.py
+++ inventory_app/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 def index(request):
 
 # Render the HTML template index.html with the data in the context variable.
-   return HttpResponse('Next Step: add content to this page(html file)\nviews.py ')
+   return HttpResponse('Next Step: add content to this page(html file)\nviews.py testingpull')
 
    
