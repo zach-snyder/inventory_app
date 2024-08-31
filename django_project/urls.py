6130a6864cbdf2250331bf70b04d43b42ea0036e
zach-snyder <zach@Zacharys-Laptop>
2024-08-30 23:19:39 -0600

    registration_working

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
